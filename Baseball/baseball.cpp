339d34b61a9998fbcef8192855d73ab3ad3ebe1d
kiho <kiho.kil@gmail.com>
2023-06-02 09:39:40 +0900

    [feature/tdd] 1st

--- Baseball/baseball.cpp
+++ Baseball/baseball.cpp
@@ -1,3 +1,64 @@
-class Baseball {
+#include <stdexcept>
+#include <string>
+using namespace std;
 
+struct Result
+{
+	bool solved;
+	int strikes;
+	int balls;
+};
+
+class Baseball {
+public:
+
+	Baseball(){}
+	explicit Baseball(string& q) : question(q) {}
+
+	auto get_value(string& length)
+	{
+		return length[0] == length[1] ||
+			length[0] == length[2] ||
+			length[1] == length[2];
+	}
+
+	bool isNonNumbers(string& length)
+	{
+		
+	}
+
+	void isIllegalArgument(string guessNumber)
+	{
+		if(guessNumber == "")
+			throw std::invalid_argument("값을 넣어야 함");
+
+		if (guessNumber.length() != 3)
+			throw std::invalid_argument("자리수 오류");
+
+		for(char ch : guessNumber)
+		{
+			if (!(ch >= '0' && ch <= '9')) continue;
+			throw std::invalid_argument("숫자로만");
+		}
+
+		//if(isNonNumber(guessNumber)) throw std::invalid_argument("숫자로만");
+
+
+		if (get_value(guessNumber))
+		{
+			throw std::invalid_argument("중복수 금지");
+		}
+	}
+
+	Result guess(string guessNumber)
+	{
+		isIllegalArgument(guessNumber);
+
+		Result result = { true,3,0 };
+
+		return result;
+	}
+
+private:
+	string question;
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,22 @@
 #include "pch.h"
 #include "../Baseball/baseball.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
+TEST(BaseballGame, InvalidArgument) {
+
+	Baseball baseball;
+
+	EXPECT_THROW(baseball.guess(""), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("1543"), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("1v3"), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("121"), std::invalid_argument);
+}
+
+TEST(BaseballGame, PerfectMatch) {
+	string answer = "123";
+	Baseball baseball(answer);
+	Result result = baseball.guess(answer);
+
+	EXPECT_EQ(true, result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
