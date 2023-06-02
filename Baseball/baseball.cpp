6b6ce0bbf248ece7eb2717d91b5fb8e1841e8218
kiho <kiho.kil@gmail.com>
2023-06-02 10:19:46 +0900

    [feature/tdd] 3rd MissMatch

--- Baseball/baseball.cpp
+++ Baseball/baseball.cpp
@@ -32,36 +32,57 @@ public:
 
 	void isIllegalArgument(string guessNumber)
 	{
-		if(guessNumber == "")
-			throw std::invalid_argument("값을 넣어야 함");
-
-		if (guessNumber.length() != 3)
-			throw std::invalid_argument("자리수 오류");
-
-		//for(char ch : guessNumber)
-		//{
-		//	if (!(ch >= '0' && ch <= '9')) continue;
-		//	throw std::invalid_argument("숫자로만");
-		//}
-
+		if(guessNumber == "")	throw std::invalid_argument("값을 넣어야 함");
+		if (guessNumber.length() != 3)	throw std::invalid_argument("자리수 오류");
 		if(isIncludeChar(guessNumber)) throw std::invalid_argument("숫자로만");
+		if (isDuplicateNumber(guessNumber))	throw std::invalid_argument("중복수 금지");
+	}
 
-
-		if (isDuplicateNumber(guessNumber))
-		{
-			throw std::invalid_argument("중복수 금지");
-		}
+	bool getResultSuccess(int strike)
+	{
+		if (strike != 3) return false;
+		return true;
 	}
 
 	Result guess(string guessNumber)
 	{
 		isIllegalArgument(guessNumber);
 
-		Result result = { true,3,0 };
+		int numStrikes = getNumStrkes(guessNumber);
+		int numBall = getNumBall(guessNumber);
 
+		Result result = { getResultSuccess(numStrikes),numStrikes,numBall };
 		return result;
 	}
 
 private:
+	int getNumStrkes(string guessNumber)
+	{
+		int result = 0;
+		for (int i = 0; i < 3;++i)
+		{
+			int index = question.find(guessNumber[i]);
+			if (index == -1)continue;
+			if (index == i)
+			{
+				result++;
+				continue;
+			}
+		}
+		return result;
+	}
+
+	int getNumBall(string guessNumber)
+	{
+		int result = 0;
+		for (int i = 0; i < 3;++i)
+		{
+			int index = question.find(guessNumber[i]);
+			if (index == -1)continue;
+			if (index == i) continue;
+			result++;
+		}
+		return result;
+	}
 	string question;
 };
